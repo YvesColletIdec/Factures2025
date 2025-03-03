60b4f0c07ec27978e81adc65b73e48d5eeedad09
YvesColletIdec <yves.collet.idec@gmail.com>
2025-03-03 21:13:14 +0100

    ajout sql

---
+++ insertarticles.sql
@@ -0,0 +1,3 @@
+INSERT INTO Article (Nom, Prix) VALUES ('intel core vpro', 1234.55);
+INSERT INTO Article (Nom, Prix) VALUES ('amd athlon', 234.90);
+INSERT INTO Article (Nom, Prix) VALUES ('intel i7', 450);
